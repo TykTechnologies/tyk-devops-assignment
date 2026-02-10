ac251c00022f155f0d8725ca491d3fb5aee90e9a
Asutosh <asutosh@tyk.io>
2026-02-10 14:41:00 +0530

    Minor cosmetic changes

--- cmd/httpbin/main.go
+++ cmd/httpbin/main.go
@@ -13,11 +13,32 @@ import (
 	"github.com/TykTechnologies/tyk-devops-assignement/internal/server"
 )
 
+// Build information - set via ldflags during build
+var (
+	version   = "dev"
+	commit    = "unknown"
+	buildTime = "unknown"
+)
+
+// printVersion prints version information
+func printVersion() {
+	fmt.Printf("\tVersion:      %s\n", version)
+	fmt.Printf("\tCommit:       %s\n", commit)
+	fmt.Printf("\tBuild time:   %s\n", buildTime)
+}
+
 func main() {
 	// Parse command-line flags
 	host := flag.String("host", "0.0.0.0", "Host to bind the server to")
 	port := flag.Int("port", 8080, "Port to bind the server to")
+	showVersion := flag.Bool("version", false, "Show version information")
 	flag.Parse()
+
+	// Show version and exit if requested
+	if *showVersion {
+		printVersion()
+		os.Exit(0)
+	}
 
 	// Create server
 	addr := fmt.Sprintf("%s:%d", *host, *port)
@@ -25,7 +46,7 @@ func main() {
 
 	// Start server in a goroutine
 	go func() {
-		log.Printf("Starting httpbin server on %s", addr)
+		log.Printf("Starting httpbin server on %s (version: %s, commit: %s)", addr, version, commit)
 		if err := srv.Start(); err != nil {
 			log.Fatalf("Server failed to start: %v", err)
 		}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/TykTechnologies/tyk-devops-assignement
 
-go 1.25.5
+go 1.25.7
